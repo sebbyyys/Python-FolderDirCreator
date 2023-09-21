a5a51e1979243d3e4d9a01d9c1d7f4f91e65c545
sebbyyys <sebaszwed@outlook.com>
2023-09-22 00:30:58 +0100

    Built the Exe and made some tweaks to formatting
    GUI to be added (possibly)

--- FolderMaker.py
+++ FolderMaker.py
@@ -2,19 +2,18 @@ import os
 import os.path
 import time
 
-illegal = '<>:"/\|?*'
-yesInputs = ["yes", "Yes", "y", "Y"]
-noInputs = ["no", "No", "n", "N"]
+illegal = '<>:"/\|?* '
+yesInputs = ["yes", "Yes", "y", "Y"," yes","yes "]
 name = "yes"
 
 
 while name in yesInputs:
     name = ""
-    name = input("Would you like to use the program? Yes or no?")
-    if name not in noInputs:
+    name = input("\nWould you like to use the program? Yes or no/exit? ")
+    if name in yesInputs:
         ###root_path = 'E:\FolderMaker\Folders'
-        root_path = input("Please copy and paste in the filepath of where you want to make folders: ")
-        folders = input("Please input each folder name followed by a semicolon:")
+        root_path = input("\nPlease copy and paste in the filepath of where you want to make folders: ")
+        folders = input("\nPlease input each folder name followed by a semicolon. (1;2;3;4 is a valid input): ")
             
         # checks if the directory path specified is a correct one
         check = os.path.isdir(root_path)
@@ -23,7 +22,7 @@ while name in yesInputs:
         # error checking all of following code
         try:
             if check == False:
-                raise(TypeError)
+                raise(LookupError)
         # checks if any characters in the folder input are illegal (cant be added to folder name)
             for x in folders:
                 if x not in illegal:
@@ -31,11 +30,11 @@ while name in yesInputs:
                 else:
         # if characters are illegal then make the emptyString variable blank and  raise an error which will stop program carrying on
                     emptyString = ""
-                    raise(NameError)
+                    raise(MemoryError)
 
         # checks to see if the emptyString variable is empty, if it is it returns error so no single folders are made
             if emptyString == "":
-                raise(NameError)
+                raise(MemoryError)
         # otherwise takes the string splits it between colons and makes it into a list, joins the path and list together and makes a directory for each item in the list
             else:
                 finalList = emptyString.split(";")
@@ -45,17 +44,17 @@ while name in yesInputs:
                 listnames = ''
                 for x in finalList:
                     listnames += ' ' + x
-                print(f"Successfully created folders{listnames[0:-1]}and {listnames[-1:]}")
+                print(f"\nSuccessfully created folders{listnames[0:-1]}and {listnames[-1:]}")
                 time.sleep(5) 
                 
-        except NameError:
-            print("Illegal character detected \n""\n""None of the following characters are allowed: \n""\n"""' < > : " / \ | ? * '"")
+        except MemoryError:
+            print("\nIllegal character detected \n""\n""None of the following characters are allowed: \n""\n"""' < > : " / \ | ? * '"")
             time.sleep(5)
-        except TypeError:
-            print("Wrong Filepath. Please input the correct filepath")
+        except LookupError:
+            print("\nWrong Filepath. Please input the correct filepath ")
             time.sleep(5)
         except FileExistsError:
-            print("One of those files already exists please make sure they are all unique or rename it to something else")
+            print("\nOne of those files already exists please make sure they are all unique or rename it to something else ")
             time.sleep(5)
     else:
         break
